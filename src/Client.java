8884e6be547fbf6b9aad5edc0442bd50109fbbe0
Loiev Roman <robolito0420@gmail.com>
2024-10-25 03:02:56 +0300

    Added authentication

--- src/Client.java
+++ src/Client.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Client {
     private DatagramSocket clientSocket;
@@ -25,12 +24,12 @@ public class Client {
                 continue;
             }
             System.out.println("Підключення успішне!");
-            while (menu());
+            menu();
             break;
         }
     }
 
-    private boolean menu() throws Exception {
+    private void menu() throws Exception {
         String choice;
         boolean isAuth = false;
         while (!isAuth) {
@@ -56,37 +55,40 @@ public class Client {
                     break;
                 case "0":
                     System.out.println("Вихід з програми.");
-                    return false;
+                    return;
                 default:
                     System.out.println("Невірний вибір, спробуйте ще раз.");
             }
+
+            boolean exitStatus = false;
+            while (!exitStatus) {
+                System.out.println("=== Консольне меню ===");
+
+                System.out.println("1. Підключитись до іншого клієнта");
+                System.out.println("2. Надати підключення до свого клієнта");
+                System.out.println("0. Вийти");
+
+                choice = reader.readLine();
+                switch (choice) {
+                    case "1":
+                        connectToAnotherClient();
+                        break;
+                    case "2":
+                        allowConnectionToYourClient();
+                        break;
+                    case "0":
+                        exitStatus = true;
+                        isAuth = false;
+                        System.out.println("Вихід.");
+                        break;
+                    default:
+                        System.out.println("Невірний вибір, спробуйте ще раз.");
+                }
+            }
         }
-
-
-        System.out.println("=== Консольне меню ===");
-
-        System.out.println("1. Підключитись до іншого клієнта");
-        System.out.println("2. Надати підключення до свого клієнта");
-        System.out.println("0. Вийти");
-
-        choice = reader.readLine();
-        switch (choice) {
-            case "1":
-                connectToAnotherClient();
-                break;
-            case "2":
-                allowConnectionToYourClient();
-                break;
-            case "0":
-                System.out.println("Вихід з програми.");
-                return false;
-            default:
-                System.out.println("Невірний вибір, спробуйте ще раз.");
-        }
-        return true;
     }
 
-    public boolean connectToServer() {
+    private boolean connectToServer() {
         try {
             String testMessage = "PING";
             byte[] sendBuffer = testMessage.getBytes();
@@ -107,7 +109,7 @@ public class Client {
         }
     }
 
-    public boolean login() throws IOException {
+    private boolean login() throws IOException {
         System.out.println("Введіть логін:");
         String login = reader.readLine();
 
@@ -118,7 +120,7 @@ public class Client {
         sendMessageToServer(credentials);
 
         try {
-            String serverResponse = receiveMessageFromServer();
+            String serverResponse = receiveMessageFromServer(10000);
             if (serverResponse.equals("LOGIN_SUCCESS")) {
                 System.out.println("Авторизація успішна.");
                 this.login = login;
@@ -133,7 +135,7 @@ public class Client {
         }
     }
 
-    public boolean register() throws IOException {
+    private boolean register() throws IOException {
         System.out.println("Введіть новий логін:");
         String login = reader.readLine();
 
@@ -144,7 +146,7 @@ public class Client {
         sendMessageToServer(credentials);
 
         try {
-            String serverResponse = receiveMessageFromServer();
+            String serverResponse = receiveMessageFromServer(10000);
             if (serverResponse.equals("REGISTER_SUCCESS")) {
                 System.out.println("Реєстрація успішна.");
                 this.login = login;
@@ -160,11 +162,36 @@ public class Client {
     }
 
 
-    public void connectToAnotherClient() throws Exception {
+    private void connectToAnotherClient() throws Exception {
         System.out.println("Введіть IP клієнта для підключення:");
         String targetIP = reader.readLine();
         System.out.println("Введіть порт клієнта:");
         int targetPort = Integer.parseInt(reader.readLine());
+
+        String connectionRequest = "CONNECT_REQUEST:" + targetIP + ":" + targetPort + ":" + login;
+        sendMessageToServer(connectionRequest);
+
+        System.out.println("Чекаємо дозволу на підключення (30 секунд)");
+        try {
+            String serverResponse = receiveMessageFromServer(30_000);
+            String[] parts = serverResponse.split(":", 3);
+
+            if (parts[0].equals("CONNECT_DENIED")) {
+                System.out.println("Підключення відхилено.");
+                return;
+            } else if (parts[0].equals("CONNECT_ACCEPTED")) {
+                System.out.println("Підключення підтверджено. Тепер ви можете надсилати команди.");
+            } else if (parts[0].equals("NO_SUCH_USER")) {
+                System.out.println("Такого клієнту немає в системі");
+            }
+        } catch (IOException e) {
+            System.out.println("Не вдалося отримати відповідь на запит підключення.");
+            return;
+        }
+
+
+
+        // Цикл для надсилання команд
         while (true) {
             System.out.println("Введіть команду для виконання (0 to exit):");
             String command = reader.readLine();
@@ -173,53 +200,78 @@ public class Client {
             }
             sendCommandToAnotherClient(targetIP, targetPort, command);
             try {
-                String serverResponse = receiveMessageFromServer();
+                String serverResponse = receiveMessageFromServer(10000);
                 System.out.println("Результат виконання команди: " + serverResponse);
             } catch (IOException e) {
                 System.out.println("Відповідь не прийшла, перевірте підключення та повторіть спробу");
             }
-
         }
     }
 
-    public void allowConnectionToYourClient() throws Exception {
+
+    private void allowConnectionToYourClient() throws Exception {
         Thread listenerThread = new Thread(() -> {
-            System.out.println("Очікування підключень...");
+            System.out.println("Очікування запитів на підключення...");
             while (!Thread.currentThread().isInterrupted()) {
                 try {
-                    String clientMessage = receiveMessageFromServer();
-                    if (clientMessage.startsWith("EXECUTE")) {
+                    String clientMessage = receiveMessageFromServer(10000);
+                    if (clientMessage.startsWith("CONNECT_REQUEST")) {
                         String[] parts = clientMessage.split(":");
                         String senderIP = parts[1].substring(1);
                         int senderPort = Integer.parseInt(parts[2]);
-                        String command = parts[3];
+                        String senderLogin = parts[3];
 
-                        System.out.printf("Отримана команда від %s:%d : %s\n", senderIP, senderPort, command);
-
-                        String result = CommandExecutor.executeCommand(command);
-                        System.out.println(result);
-                        sendMessageToServer("RESPONSE_TO:%s:%d:%s:%s".formatted(senderIP, senderPort, command, result));
+                        System.out.printf("Клієнт %s хоче підключитися. Підтвердити підключення? (yes/no)\n", senderLogin);
+                        String userResponse = reader.readLine();
+                        if (userResponse.equals("yes")) {
+                            sendMessageToServer("CONNECT_ACCEPTED:" + senderIP + ":" + senderPort);
+                            System.out.println("Підключення підтверджено для клієнта " + senderLogin);
+                            executeFromClient(senderIP, senderPort);
+                        } else {
+                            sendMessageToServer("CONNECT_DENIED:" + senderIP + ":" + senderPort);
+                            System.out.printf("Підключення відхилено для клієнта %s\n", senderLogin);
+                        }
                     }
                 } catch (IOException e) {
-                    System.out.println("Очікування підключень...");
+                    System.out.println("Очікування запитів на підключення...");
                 }
             }
         });
 
         listenerThread.start();
+        listenerThread.join();
+//        while (true) {
+//            System.out.println("Введіть 'exit' для завершення очікування запитів на підключення:");
+//            userResponse = reader.readLine();
+//            if (userResponse.equalsIgnoreCase("exit")) {
+//                listenerThread.interrupt();
+//                listenerThread.join();
+//                System.out.println("Очікування запитів на підключення завершено.");
+//                break;
+//            }
+//        }
+    }
 
-        // В основному потоці продовжуємо читати команди
-        while (true) {
-            System.out.println("Введіть 'exit' для завершення очікування підключень:");
-            String input = reader.readLine();
-            if (input.equalsIgnoreCase("exit")) {
-                listenerThread.interrupt();
-                listenerThread.join();
-                System.out.println("Очікування підключень завершено.");
-                break;
+    private void executeFromClient(String clientIP, int clientPort){
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                String clientMessage = receiveMessageFromServer(0);
+                String[] parts = clientMessage.split(":", 4);
+                if (clientMessage.startsWith("EXECUTE") && parts[1].equals("/" + clientIP) && Integer.parseInt(parts[2]) == clientPort) {
+                    executeAndRespond(parts[1], Integer.parseInt(parts[2]), parts[3]);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
     }
+
+    private void executeAndRespond(String senderIP, int senderPort, String command) throws IOException {
+        String result = CommandExecutor.executeCommand(command);
+        System.out.println(result);
+        sendMessageToServer("RESPONSE_TO:%s:%d:%s:%s".formatted(senderIP, senderPort, command, result));
+    }
+
 
     private void sendCommandToAnotherClient(String targetIp, int targetPort, String command) throws IOException {
         String message = "SEND_TO:" + targetIp + ":" + targetPort + ":" + command;
@@ -232,10 +284,10 @@ public class Client {
         clientSocket.send(sendPacket);
     }
 
-    private String receiveMessageFromServer() throws IOException {
+    private String receiveMessageFromServer(int timeToWait) throws IOException {
         byte[] receiveBuffer = new byte[1024];
         DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
-        clientSocket.setSoTimeout(10_000);
+        clientSocket.setSoTimeout(timeToWait);
         clientSocket.receive(receivePacket);
         return new String(receivePacket.getData(), 0, receivePacket.getLength());
     }
--- src/Server.java
+++ src/Server.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,7 +26,7 @@ public class Server {
                 DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
                 serverSocket.receive(receivePacket);
 
-                String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+
                 String clientKey = receivePacket.getAddress().toString() + ":" + receivePacket.getPort();
 
                 if (!clients.containsKey(clientKey)) {
@@ -34,69 +35,135 @@ public class Server {
                     DatabaseManager.saveClient(receivePacket.getAddress().toString().substring(1), receivePacket.getPort());
                 }
 
-                // Обробка команд
-                if (clientMessage.equals("PING")) {
-                    String responseMessage = "PONG";
-                    sendMessage(receivePacket.getAddress(), receivePacket.getPort(), responseMessage);
-                    System.out.println("Sent response: PONG");
-                } else if (clientMessage.startsWith("SEND_TO:")) {
-                    String[] parts = clientMessage.split(":");
-                    String targetIP = parts[1];
-                    int targetPort = Integer.parseInt(parts[2]);
-                    String command = parts[3];
-
-                    String message = "%s:%s:%d:%s".formatted("EXECUTE", receivePacket.getAddress(), receivePacket.getPort(), command);
-                    ClientInfo targetClient = findClient(targetIP, targetPort);
-                    if (targetClient != null) {
-                        System.out.println("Sending execute to client: " + targetClient);
-                        sendMessage(targetClient, message);
-                    } else {
-                        System.out.println("Target client not found.");
-                    }
-                } else if (clientMessage.startsWith("RESPONSE_TO:")) {
-                    String[] parts = clientMessage.split(":", 5);
-                    String targetIP = parts[1];
-                    int targetPort = Integer.parseInt(parts[2]);
-                    String command = parts[3];
-                    String response = parts[4];
-                    ClientInfo targetClient = findClient(targetIP, targetPort);
-                    if (targetClient != null) {
-                        System.out.println("Sending response to client: " + targetClient);
-                        DatabaseManager.saveCommand(receivePacket.getAddress().toString().substring(1), receivePacket.getPort(),
-                                targetIP, targetPort, command, response);
-                        sendMessage(targetClient, response);
-                    } else {
-                        System.out.println("Target client not found.");
-                    }
-                } else if (clientMessage.startsWith("LOGIN:")) {
-                    String[] parts = clientMessage.split(":");
-                    String login = parts[1];
-                    String password = parts[2];
-
-                    if (DatabaseManager.isValidUser(login, password)) {
-                        sendMessage(receivePacket.getAddress(), receivePacket.getPort(), "LOGIN_SUCCESS");
-                    } else {
-                        sendMessage(receivePacket.getAddress(), receivePacket.getPort(), "LOGIN_FAILED");
-                    }
-                } else if (clientMessage.startsWith("REGISTER:")) {
-                    String[] parts = clientMessage.split(":");
-                    String login = parts[1];
-                    String password = parts[2];
-
-                    if (DatabaseManager.registerUser(login, password)) {
-                        sendMessage(receivePacket.getAddress(), receivePacket.getPort(), "REGISTER_SUCCESS");
-                    } else {
-                        sendMessage(receivePacket.getAddress(), receivePacket.getPort(), "REGISTER_FAILED");
-                    }
-                }
-                else {
-                    System.out.printf("Command \"%s\" not found\n", clientMessage);
-                }
+                handleMessage(receivePacket);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    private static void handleMessage(DatagramPacket receivePacket) throws IOException, SQLException {
+        String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        String[] parts = clientMessage.split(":", 5);
+
+        String targetIP, connectionMessage, senderLogin;
+        int targetPort;
+        ClientInfo targetClient;
+        switch (parts[0]) {
+            case "PING":
+                String responseMessage = "PONG";
+                sendMessage(receivePacket.getAddress(), receivePacket.getPort(), responseMessage);
+                System.out.println("Sent response: PONG");
+                break;
+
+            case "CONNECT_REQUEST":
+                targetIP = parts[1];
+                targetPort = Integer.parseInt(parts[2]);
+                senderLogin = parts[3];
+
+                connectionMessage = "CONNECT_REQUEST:" + receivePacket.getAddress() + ":" + receivePacket.getPort() + ":" + senderLogin;
+
+                targetClient = findClient(targetIP, targetPort);
+                if (targetClient != null) {
+                    sendMessage(targetClient, connectionMessage);
+                    System.out.printf("Forwarded connect request from %s to %s:%d\n",
+                            receivePacket.getAddress() + ":" + receivePacket.getPort(), targetIP, targetPort);
+                } else {
+                    System.out.printf("Target client %s:%d not found\n", targetIP, targetPort);
+                    sendMessage(receivePacket.getAddress(), receivePacket.getPort(), "NO_SUCH_USER");
+                }
+                break;
+            case "CONNECT_ACCEPTED":
+                targetIP = parts[1];
+                targetPort = Integer.parseInt(parts[2]);
+
+                connectionMessage = "CONNECT_ACCEPTED:" + receivePacket.getAddress() + ":" + receivePacket.getPort();
+
+                targetClient = findClient(targetIP, targetPort);
+                if (targetClient != null) {
+                    sendMessage(targetClient, connectionMessage);
+                    System.out.printf("Forwarded connect accept from %s to %s:%d\n",
+                            receivePacket.getAddress() + ":" + receivePacket.getPort(), targetIP, targetPort);
+                } else {
+                    System.out.printf("Target client %s:%d not found\n", targetIP, targetPort);
+                }
+                break;
+            case "CONNECT_DENIED":
+                targetIP = parts[1];
+                targetPort = Integer.parseInt(parts[2]);
+
+                connectionMessage = "CONNECT_DENIED:" + receivePacket.getAddress() + ":" + receivePacket.getPort();
+
+                targetClient = findClient(targetIP, targetPort);
+                if (targetClient != null) {
+                    sendMessage(targetClient, connectionMessage);
+                    System.out.printf("Forwarded connect denied from %s to %s:%d\n",
+                            receivePacket.getAddress() + ":" + receivePacket.getPort(), targetIP, targetPort);
+                } else {
+                    System.out.printf("Target client %s:%d not found\n", targetIP, targetPort);
+                }
+                break;
+
+            case "SEND_TO":
+                String sendToTargetIP = parts[1];
+                int sendToTargetPort = Integer.parseInt(parts[2]);
+                String command = parts[3];
+
+                String message = "EXECUTE:" + receivePacket.getAddress() + ":" + receivePacket.getPort() + ":" + command;
+                ClientInfo sendToClient = findClient(sendToTargetIP, sendToTargetPort);
+                if (sendToClient != null) {
+                    System.out.println("Sending execute to client: " + sendToClient);
+                    sendMessage(sendToClient, message);
+                } else {
+                    System.out.println("Target client not found.");
+                }
+                break;
+
+            case "RESPONSE_TO":
+                String responseToTargetIP = parts[1].substring(1);
+                int responseToTargetPort = Integer.parseInt(parts[2]);
+                String responseCommand = parts[3];
+                String response = parts[4];
+
+                ClientInfo responseToClient = findClient(responseToTargetIP, responseToTargetPort);
+                if (responseToClient != null) {
+                    System.out.println("Sending response to client: " + responseToClient);
+                    DatabaseManager.saveCommand(receivePacket.getAddress().toString().substring(1), receivePacket.getPort(),
+                            responseToTargetIP, responseToTargetPort, responseCommand, response);
+                    sendMessage(responseToClient, response);
+                } else {
+                    System.out.println("Target client not found: " + responseToTargetIP + ":" + responseToTargetPort);
+                }
+                break;
+
+            case "LOGIN":
+                String login = parts[1];
+                String password = parts[2];
+
+                if (DatabaseManager.isValidUser(login, password)) {
+                    sendMessage(receivePacket.getAddress(), receivePacket.getPort(), "LOGIN_SUCCESS");
+                } else {
+                    sendMessage(receivePacket.getAddress(), receivePacket.getPort(), "LOGIN_FAILED");
+                }
+                break;
+
+            case "REGISTER":
+                String registerLogin = parts[1];
+                String registerPassword = parts[2];
+
+                if (DatabaseManager.registerUser(registerLogin, registerPassword)) {
+                    sendMessage(receivePacket.getAddress(), receivePacket.getPort(), "REGISTER_SUCCESS");
+                } else {
+                    sendMessage(receivePacket.getAddress(), receivePacket.getPort(), "REGISTER_FAILED");
+                }
+                break;
+
+            default:
+                System.out.printf("Command \"%s\" not found\n", clientMessage);
+                break;
+        }
+    }
+
 
     private static ClientInfo findClient(String ip, int port) {
         ip = "/" + ip;
